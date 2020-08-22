b56b248cde032ea3c096fff3673428ce0806bdd5
ivanstnsk <ivan.stnsk@gmail.com>
2020-08-22 03:44:47 +0300

    Set up global paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,24 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            screens: "./screens",
+            components: "./components",
+            navigation: "./navigation",
+            ui: "./ui",
+            hooks: "./hooks",
+            store: "./store",
+            types: "./types",
+            constants: "./constants",
+            assets: "./assets",
+            api: "./api",
+          }
+        }
+      ]
+    ]
   };
 };
