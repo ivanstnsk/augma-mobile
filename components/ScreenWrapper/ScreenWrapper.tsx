45d172010c0b4d6b9e07b1bc8a497b626a619ce4
ivanstnsk <ivan.stnsk@gmail.com>
2020-08-23 03:17:52 +0300

    Update ScreenWrapper. Fix insets

--- components/ScreenWrapper/ScreenWrapper.tsx
+++ components/ScreenWrapper/ScreenWrapper.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import { useSafeAreaInsets, useSafeAreaFrame } from 'react-native-safe-area-context';
 
 type Props = {
   children: React.ReactElement | React.ReactElement[];
@@ -8,9 +9,31 @@ type Props = {
 export const ScreenWrapper: React.FC<Props> = ({
   children,
 }) => {
+  const insets = useSafeAreaInsets();
+  const frame = useSafeAreaFrame();
+  const minHeight = frame.height - insets.bottom - insets.top;
+
+  const containerStyles = [
+    styles.container,
+    {
+      marginTop: insets.top,
+      marginBottom: insets.bottom,
+    }
+  ];
+
+  const innerStyles = [
+    styles.inner,
+    { minHeight }
+  ];
+
   return (
     <View style={styles.wrapper}>
-      {children}
+      <ScrollView
+        style={containerStyles}
+        contentContainerStyle={innerStyles}
+      >
+        {children}
+      </ScrollView>
     </View>
   );
 }
@@ -18,7 +41,13 @@ export const ScreenWrapper: React.FC<Props> = ({
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: '#1F1F1F',
   },
+  container: {
+    flex: 1,
+  },
+  inner: {
+    flexDirection: 'column',
+    paddingHorizontal: 20,
+  }
 });
