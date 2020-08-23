6d990f03b91d52559fde1f095780d6f28f1afa63
ivanstnsk <ivan.stnsk@gmail.com>
2020-08-23 03:17:22 +0300

    Update Button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,23 +1,50 @@
 import * as React from 'react';
 import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 
+type ButtonLookVariant = 'normal' | 'outline';
+
 type Props = {
   label: string;
+  variant?: ButtonLookVariant;
   onPress: () => void;
+}
+
+const getStyles = (variant: ButtonLookVariant) => {
+  switch (variant) {
+    case 'outline': {
+      return {
+        button: [
+          styles.button,
+          {
+            backgroundColor: 'transparent',
+            borderColor: '#919191',
+          },
+        ],
+        label: [
+          styles.label,
+          { color: '#919191' },
+        ],
+      };
+    }
+    case 'normal': default: return styles;
+  }
 }
 
 export const Button: React.FC<Props> = ({
   label,
+  variant = 'normal',
   onPress,
 }) => {
+  const preparedStyles = getStyles(variant);
+
   return (
     <TouchableOpacity
       onPress={onPress}
-      style={styles.button}
+      style={preparedStyles.button}
     >
       <Text
         numberOfLines={1}
-        style={styles.label}
+        style={preparedStyles.label}
       >
         {label}
       </Text>
@@ -27,13 +54,17 @@ export const Button: React.FC<Props> = ({
 
 const styles = StyleSheet.create({
   button: {
-    width: '100%',
+    flex: 1,
+    height: 'auto',
+    maxHeight: 56,
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 16,
     paddingVertical: 16,
     backgroundColor: '#C62626',
     borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#C62626',
   },
   label: {
     fontSize: 16,
