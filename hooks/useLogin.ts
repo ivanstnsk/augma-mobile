3cbf717a81bb45c94f5702c2f9a6c0957804045c
ivanstnsk <ivan.stnsk@gmail.com>
2020-08-24 22:42:29 +0300

    Fix useLogin hook

--- hooks/useLogin.ts
+++ hooks/useLogin.ts
@@ -6,7 +6,7 @@ import { useLoader } from 'hooks/useLoader';
 
 type LoginHook = {
   userType: UserType;
-  login: (data: UserCredentials) => void;
+  login: (data: UserCredentials) => Promise<void>;
   registration: (data: UserCredentials) => void;
   logout: () => void;
 }
@@ -16,10 +16,17 @@ export const useLogin = (): LoginHook => {
   const Loader = useLoader();
   const userType = User.useUserType();
 
-  const login = async (data: UserCredentials) => {
-    Loader.show();
-    await User.login(data, dispatch);
-    Loader.hide();
+  const login = async (data: UserCredentials): Promise<void> => {
+    return new Promise(async (resolve, reject) => {
+      Loader.show();
+      try {
+        await User.login(data, dispatch);
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+      Loader.hide();
+    });
   }
 
   const registration = async (data: UserCredentials) => {
