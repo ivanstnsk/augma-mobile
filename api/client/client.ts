391fd9bd7b2e99b3cf78c42ce937dfefa5cb0603
ivanstnsk <ivan.stnsk@gmail.com>
2020-08-27 03:13:12 +0300

    Fix client api host

--- api/client/client.ts
+++ api/client/client.ts
@@ -1,6 +1,6 @@
 import Axios, { AxiosResponse, AxiosError, AxiosRequestConfig } from 'axios';
 
-const BASE_URL = 'http://localhost:3000/';
+const BASE_URL = 'http://192.168.0.102:3000/';
 
 export const client = Axios.create({
   baseURL: BASE_URL,
