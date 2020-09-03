3d12175b9abdc0bbc83b1abaecc1db652d36e86f
ivanstnsk <ivan.stnsk@gmail.com>
2020-09-03 19:52:16 +0300

    Update QuestProgress

--- screens/User/screens/QuestProgress/QuestProgress.tsx
+++ screens/User/screens/QuestProgress/QuestProgress.tsx
@@ -1,16 +1,44 @@
 import * as React from 'react';
-// import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import { ScreenWrapper } from 'components/ScreenWrapper';
-import { NoScreen } from 'components/NoScreen';
+import { Button } from 'components/Button';
+import { useQuestActions } from 'store/quest';
 
 export const QuestProgress: React.FC = () => {
+  const QuestActions = useQuestActions();
+
+  const handleFinishPress = () => {
+    QuestActions.finish();
+  }
+
   return (
-    <ScreenWrapper>
-      <NoScreen />
-    </ScreenWrapper>
+    <View style={styles.wrapper}>
+      <View style={styles.infoContainer}>
+      </View>
+      <Button
+        onPress={handleFinishPress}
+        label="Завершить квест"
+        variant="outline"
+        color="primary"
+      />
+    </View>
   );
 }
 
-// const styles = StyleSheet.create({
-// });
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+    backgroundColor: '#1F1F1F',
+    paddingHorizontal: 20,
+    paddingTop: 16,
+    paddingBottom: 19,
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+});
