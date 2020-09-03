d3b1986f5e21b090ae750dcd34047c38acb45d7b
ivanstnsk <ivan.stnsk@gmail.com>
2020-09-03 19:51:49 +0300

    Update ui / styles

--- ui/styles.ts
+++ ui/styles.ts
@@ -1,3 +1,5 @@
+import { StackCardInterpolationProps } from '@react-navigation/stack';
+
 export const headerSecondaryStyles = {
   headerStyle: {
     backgroundColor: '#1F1F1F',
@@ -6,4 +8,29 @@ export const headerSecondaryStyles = {
   },
   headerTintColor: 'rgba(255,255,255,0.4)',
   headerBackTitle: ' ',
+};
+
+export const cardStyleInterpolator = (props: StackCardInterpolationProps) => {
+  const { current: { progress } } = props;
+  const cardTranslateY = progress.interpolate({
+    inputRange: [0, 1],
+    outputRange: [500, 0],
+    extrapolate: 'clamp',
+  });
+  return ({
+    cardStyle: {
+      opacity: progress.interpolate({
+        inputRange: [0, 0.5, 0.9, 1],
+        outputRange: [0, 0.25, 0.7, 1],
+      }),
+      transform: [{ translateY: cardTranslateY }],
+    },
+    overlayStyle: {
+      opacity: progress.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 0.57],
+        extrapolate: 'clamp',
+      }),
+    },
+  });
 };
