484216b1d10511b015a6865f1029fe457084e5e1
ivanstnsk <ivan.stnsk@gmail.com>
2020-09-11 01:18:55 +0300

    Update QuestProgress

--- screens/User/screens/QuestProgress/QuestProgress.tsx
+++ screens/User/screens/QuestProgress/QuestProgress.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 import { Button } from 'components/Button';
 import { useQuestActions } from 'store/quest';
+
+import { QuestPointCard } from './components';
 
 export const QuestProgress: React.FC = () => {
   const QuestActions = useQuestActions();
@@ -13,8 +15,58 @@ export const QuestProgress: React.FC = () => {
 
   return (
     <View style={styles.wrapper}>
-      <View style={styles.infoContainer}>
-      </View>
+      <ScrollView
+        contentContainerStyle={styles.infoContainer}
+      >
+        <QuestPointCard
+          data={{
+            id: 1,
+            title: 'test',
+            description: 'test',
+            type: 'passed'
+          }}
+        />
+        <QuestPointCard
+          data={{
+            id: 2,
+            title: 'test',
+            description: 'test',
+            type: 'active'
+          }}
+        />
+        <QuestPointCard
+          data={{
+            id: 3,
+            title: 'test',
+            description: 'test',
+            type: 'locked'
+          }}
+        />
+        <QuestPointCard
+          data={{
+            id: 4,
+            title: 'test',
+            description: 'test',
+            type: 'locked'
+          }}
+        />
+        <QuestPointCard
+          data={{
+            id: 5,
+            title: 'test',
+            description: 'test',
+            type: 'locked'
+          }}
+        />
+        <QuestPointCard
+          data={{
+            id: 6,
+            title: 'test',
+            description: 'test',
+            type: 'locked'
+          }}
+        />
+      </ScrollView>
       <Button
         onPress={handleFinishPress}
         label="Завершить квест"
@@ -37,7 +89,6 @@ const styles = StyleSheet.create({
     paddingBottom: 19,
   },
   infoContainer: {
-    flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
   },
