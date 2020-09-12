1ebc3aaaf85b7df1e48380379e775b499a8cb1a6
ivanstnsk <ivan.stnsk@gmail.com>
2020-09-12 14:09:14 +0300

    Update store. Add tutorials / Remove preferences

--- store/store.ts
+++ store/store.ts
@@ -1,21 +1,33 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import * as Redux from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 
 import { userReducer } from './user/userReducer';
-import { preferencesReducer } from './preferences/preferencesReducer';
 import { questsReducer } from './quests/questsReducer';
 import { globalErrorReducer } from './globalError/globalErrorReducer';
 import { questReducer } from './quest/questReducer';
 import { sessionReducer } from './session/sessionReducer';
+import { tutorialsReducer } from './tutorials/tutorialsReducer';
 
 import { middlewares } from './middlewares'
 
+const questConfig = {
+  key: 'quest',
+  storage: AsyncStorage,
+};
+
+const tutorialsConfig = {
+  key: 'tutorials',
+  storage: AsyncStorage,
+};
+
 const rootReducer = Redux.combineReducers({
   user: userReducer,
-  preferences: preferencesReducer,
   quests: questsReducer,
   globalError: globalErrorReducer,
-  quest: questReducer,
+  quest: persistReducer(questConfig, questReducer),
   session: sessionReducer,
+  tutorials: persistReducer(tutorialsConfig, tutorialsReducer),
 });
 
 export type RootStore = ReturnType<typeof rootReducer>;
@@ -29,3 +41,5 @@ export const store = Redux.createStore(
   rootReducer,
   composeEnhancers(Redux.applyMiddleware(...middlewares)),
 );
+
+export const persistor = persistStore(store);
