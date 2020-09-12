26871eb0cbf7e924f4545a410c07f46b4322d58e
ivanstnsk <ivan.stnsk@gmail.com>
2020-09-12 14:09:40 +0300

    Add redirect to auth if 401

--- components/GlobalErrorWrapper/GlobalErrorWrapper.tsx
+++ components/GlobalErrorWrapper/GlobalErrorWrapper.tsx
@@ -2,13 +2,19 @@ import * as React from 'react';
 import { Alert } from 'react-native';
 
 import * as GlobalError from 'store/globalError';
+import * as SessionAll from 'store/session';
 
 export const GlobalErrorWrapper: React.FC = ({ children }) => {
   const Error = GlobalError.useGlobalError();
+  const SessionActions = SessionAll.useSessionActions();
 
   if (Error.error) {
     Alert.alert('Произошла ошибка');
     Error.handle();
+  }
+
+  if (Error.error?.response?.status === 401) {
+    SessionActions.logout();
   }
 
   return <>{children}</>;
