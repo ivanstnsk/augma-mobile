f314e8ed3233701bb2642840382bbe900f3046ad
ivanstnsk <ivan.stnsk@gmail.com>
2020-09-12 16:18:23 +0300

    Update QuestProgress

--- screens/User/screens/QuestProgress/QuestProgress.tsx
+++ screens/User/screens/QuestProgress/QuestProgress.tsx
@@ -1,78 +1,94 @@
 import * as React from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+import { HeaderBlock } from 'components/HeaderBlock';
 import { Button } from 'components/Button';
 import { useQuestActions } from 'store/quest';
 
-import { QuestPointCard } from './components';
+import { QuestPointCard, ProgressBar } from './components';
 
 export const QuestProgress: React.FC = () => {
+  const navigation = useNavigation();
   const QuestActions = useQuestActions();
 
   const handleFinishPress = () => {
     QuestActions.finish();
-  }
+  };
+
+  const getOpenDetailsPressHandler = React.useCallback((pointId: number) => () => {
+    navigation.navigate('questPointDetails');
+  }, []);
 
   return (
     <View style={styles.wrapper}>
+      <HeaderBlock title="Прогресс" />
       <ScrollView
         contentContainerStyle={styles.infoContainer}
       >
         <QuestPointCard
           data={{
             id: 1,
-            title: 'test',
-            description: 'test',
+            title: 'Передача данных',
+            description: 'Курьер ждет тебя на Южном вокзале. Но будь осторожен, вчера был крупный слив данных так что тебе придется хорошенько постараться если ты не хочешь попасть в ситуацию когда даже мы не сможем помочь тебе',
             type: 'passed'
           }}
+          onOpenDetailsPress={getOpenDetailsPressHandler(1)}
         />
+        <ProgressBar progress={0.3} />
         <QuestPointCard
           data={{
             id: 2,
-            title: 'test',
-            description: 'test',
+            title: 'Передача данных',
+            description: 'Курьер ждет тебя на Южном вокзале. Но будь осторожен, вчера был крупный слив данных так что тебе придется хорошенько постараться если ты не хочешь попасть в ситуацию когда даже мы не сможем помочь тебе',
             type: 'active'
           }}
+          onOpenDetailsPress={getOpenDetailsPressHandler(1)}
         />
         <QuestPointCard
           data={{
             id: 3,
-            title: 'test',
-            description: 'test',
+            title: 'Передача данных',
+            description: 'Курьер ждет тебя на Южном вокзале. Но будь осторожен, вчера был крупный слив данных так что тебе придется хорошенько постараться если ты не хочешь попасть в ситуацию когда даже мы не сможем помочь тебе',
             type: 'locked'
           }}
+          onOpenDetailsPress={getOpenDetailsPressHandler(1)}
         />
         <QuestPointCard
           data={{
             id: 4,
-            title: 'test',
-            description: 'test',
+            title: 'Передача данных',
+            description: 'Курьер ждет тебя на Южном вокзале. Но будь осторожен, вчера был крупный слив данных так что тебе придется хорошенько постараться если ты не хочешь попасть в ситуацию когда даже мы не сможем помочь тебе',
             type: 'locked'
           }}
+          onOpenDetailsPress={getOpenDetailsPressHandler(1)}
         />
         <QuestPointCard
           data={{
             id: 5,
-            title: 'test',
-            description: 'test',
+            title: 'Передача данных',
+            description: 'Курьер ждет тебя на Южном вокзале. Но будь осторожен, вчера был крупный слив данных так что тебе придется хорошенько постараться если ты не хочешь попасть в ситуацию когда даже мы не сможем помочь тебе',
             type: 'locked'
           }}
+          onOpenDetailsPress={getOpenDetailsPressHandler(1)}
         />
         <QuestPointCard
           data={{
             id: 6,
-            title: 'test',
-            description: 'test',
+            title: 'Передача данных',
+            description: 'Курьер ждет тебя на Южном вокзале. Но будь осторожен, вчера был крупный слив данных так что тебе придется хорошенько постараться если ты не хочешь попасть в ситуацию когда даже мы не сможем помочь тебе',
             type: 'locked'
           }}
+          onOpenDetailsPress={getOpenDetailsPressHandler(1)}
+        />
+        <Button
+          onPress={handleFinishPress}
+          label="Завершить квест"
+          variant="outline"
+          color="primary"
+          style={styles.finishButton}
         />
       </ScrollView>
-      <Button
-        onPress={handleFinishPress}
-        label="Завершить квест"
-        variant="outline"
-        color="primary"
-      />
     </View>
   );
 }
@@ -84,12 +100,16 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     justifyContent: 'flex-start',
     backgroundColor: '#1F1F1F',
-    paddingHorizontal: 20,
-    paddingTop: 16,
-    paddingBottom: 19,
   },
   infoContainer: {
     flexDirection: 'column',
     alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 16,
+    paddingBottom: 24,
   },
+  finishButton: {
+    width: '100%',
+    marginTop: 24,
+  }
 });
