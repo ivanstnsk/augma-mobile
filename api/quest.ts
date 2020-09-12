e6dcb063ef8c032811258622b098ee698f030bb9
ivanstnsk <ivan.stnsk@gmail.com>
2020-09-12 17:04:04 +0300

    Update api

--- api/quest.ts
+++ api/quest.ts
@@ -2,6 +2,12 @@ import { Quests } from 'types/models/Quests';
 
 import { client } from './client';
 
+export const questInfo = (questId: string, params: Quests.Request.QuestInfo) => {
+  type Req = Quests.Request.QuestInfo;
+  type Res = Quests.Response.QuestInfo;
+  return client.get<Req, Res>(`/v1/quest/${questId}/info`, { params });
+}
+
 export const questPoints = (questId: string, params: Quests.Request.QuestPoints) => {
   type Req = Quests.Request.QuestPoints;
   type Res = Quests.Response.QuestPoints;
